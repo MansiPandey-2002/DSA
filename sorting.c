6c7be54ea4e1b7f7f1cb66807ad2dce72d936a94
Mansi Pandey <125549121+MansiPandey-2002@users.noreply.github.com>
2023-09-12 13:19:44 +0000

    Bubble sort

---
+++ sorting.c
@@ -0,0 +1,17 @@
+//1-->BUBBLE Sort
+void bubble(int *a,int n)
+{
+    int i,j,temp;
+    for(i=0;i<(n-1);i++)   
+    {
+        for(j=0;j<(n-1-i);j++)  
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+}
